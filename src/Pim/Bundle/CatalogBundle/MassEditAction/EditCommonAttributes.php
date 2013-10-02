00566daa867e833493ce7ef4e3a035fd7d2e33c4
Antoine Guigan <antoine@akeneo.com>
2013-10-02 16:04:16 +0200

    Created a service for LocalizedCollectionType

--- src/Pim/Bundle/CatalogBundle/MassEditAction/EditCommonAttributes.php
+++ src/Pim/Bundle/CatalogBundle/MassEditAction/EditCommonAttributes.php
@@ -137,7 +137,7 @@ class EditCommonAttributes extends AbstractMassEditAction
     public function getFormOptions()
     {
         return array(
-            'locales'          => $this->localeManager->getActiveLocales(),
+            'locales'          => $this->localeManager->getUserLocales(),
             'commonAttributes' => $this->commonAttributes,
         );
     }
