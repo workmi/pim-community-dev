c14d151cebffbf8a513882087a0f3d9b8ffd33ee
willy-ahva <willy.mesnage@akeneo.com>
2015-03-16 14:22:42 +0100

    PIM-3896: Add spec to CSV writers

--- spec/Pim/Bundle/BaseConnectorBundle/Writer/File/CsvProductWriterSpec.php
+++ spec/Pim/Bundle/BaseConnectorBundle/Writer/File/CsvProductWriterSpec.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace spec\Pim\Bundle\BaseConnectorBundle\Writer\File;
+
 use Akeneo\Bundle\BatchBundle\Entity\StepExecution;
 use PhpSpec\ObjectBehavior;
 use Pim\Bundle\CatalogBundle\Model\ProductMediaInterface;
@@ -10,6 +11,52 @@ class CsvProductWriterSpec extends ObjectBehavior
     function let(StepExecution $stepExecution)
     {
         $this->setStepExecution($stepExecution);
+    }
+
+    function it_gives_configuration_fields()
+    {
+        $this->getConfigurationFields()->shouldReturn([
+            'filePath' => [
+                'options' => [
+                    'label' => 'pim_base_connector.export.filePath.label',
+                    'help'  => 'pim_base_connector.export.filePath.help'
+                ]
+            ],
+            'delimiter' => [
+                'options' => [
+                    'label' => 'pim_base_connector.export.delimiter.label',
+                    'help'  => 'pim_base_connector.export.delimiter.help'
+                ]
+            ],
+            'enclosure' => [
+                'options' => [
+                    'label' => 'pim_base_connector.export.enclosure.label',
+                    'help'  => 'pim_base_connector.export.enclosure.help'
+                ]
+            ],
+            'withHeader' => [
+                'type' => 'switch',
+                'options' => [
+                    'label' => 'pim_base_connector.export.withHeader.label',
+                    'help'  => 'pim_base_connector.export.withHeader.help'
+                ]
+            ]
+        ]);
+    }
+
+    function it_is_configurable()
+    {
+        $this->getDelimiter()->shouldReturn(';');
+        $this->getEnclosure()->shouldReturn('"');
+        $this->isWithHeader()->shouldReturn(true);
+
+        $this->setDelimiter(',');
+        $this->setEnclosure('^');
+        $this->setWithHeader(false);
+
+        $this->getDelimiter()->shouldReturn(',');
+        $this->getEnclosure()->shouldReturn('^');
+        $this->isWithHeader()->shouldReturn(false);
     }
 
     function it_writes_product_data_in_file_and_copy_medias($stepExecution)
